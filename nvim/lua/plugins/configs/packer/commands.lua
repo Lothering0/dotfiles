0acb415a54645f026a14ec234b904c9d7e3a517b
Lothering0 <danil2005232@gmail.com>
2024-04-07 18:32:53 +0300

    Neovim: PackerFastSnapshot command

---
+++ nvim/lua/plugins/configs/packer/commands.lua
@@ -0,0 +1,11 @@
+local packer = require("packer")
+
+-- Command for fast creating a snapshot. Path: ~/dotfiles/nvim/snapshots/YYYY-MM-DDTHH-mm-ss
+vim.api.nvim_create_user_command(
+  "PackerFastSnapshot",
+  function()
+    local file_name = os.date("%Y-%m-%dT%H-%M-%S")
+    packer.snapshot(file_name)
+  end,
+  { nargs = 0 }
+)
