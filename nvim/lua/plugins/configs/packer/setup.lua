f84ca39671dc97b7350904d968185c80840acc0c
Lothering0 <danil2005232@gmail.com>
2024-11-14 23:44:02 +0300

    Neovim: update plugins

--- nvim/lua/plugins/configs/packer/setup.lua
+++ nvim/lua/plugins/configs/packer/setup.lua
@@ -11,41 +11,78 @@ return packer.startup(function()
   -- Packer can manage itself
   use "wbthomason/packer.nvim"
 
-  -- You add plugins here  
-  use { "nvim-lualine/lualine.nvim" }                    -- Bottom line
-  use { "nvim-tree/nvim-web-devicons" }                  -- Another icons
-  use { "nvim-tree/nvim-tree.lua" }                      -- Explorer
-  -- use { "romgrk/barbar.nvim" }                        -- Tabs
+  -- Bottom line  
+  use { "nvim-lualine/lualine.nvim" }
+
+  -- Another icons
+  use { "nvim-tree/nvim-web-devicons" }
+
+  -- Explorer
+  use { "nvim-tree/nvim-tree.lua" }
+
+  -- Buffers
   use {
     "akinsho/bufferline.nvim",
     tag = "*",
     requires = "nvim-tree/nvim-web-devicons",
-  }                                                      -- Buffers
-  use { "famiu/bufdelete.nvim" }                         -- Close buffer
-  use { "phaazon/hop.nvim", branch = "v2" }              -- EasyMotion analog
-  use { "mg979/vim-visual-multi", branch = "master" }    -- Multi cursor
-  use { "marko-cerovac/material.nvim" }                  -- FIRST Material theme
-  use { "norcalli/nvim-colorizer.lua" }                  -- Color (hex/rgb) highlight
-  use { "lukas-reineke/indent-blankline.nvim" }          -- Highlight indentations
-  use { "lukas-reineke/virt-column.nvim" }               -- Set symbol on certain column
+  }
+
+  -- Close buffer
+  use { "famiu/bufdelete.nvim" }
+
+  -- EasyMotion analog
+  use { "phaazon/hop.nvim", branch = "v2" }
+
+  -- Multi cursor
+  use { "mg979/vim-visual-multi", branch = "master" }
+
+  -- Material theme
+  use { "marko-cerovac/material.nvim" }
+
+  -- Color (hex/rgb) highlight
+  use { "norcalli/nvim-colorizer.lua" }
+
+  -- Highlight indentations
+  use { "lukas-reineke/indent-blankline.nvim" }
+
+  -- Set symbol on certain column
+  use { "lukas-reineke/virt-column.nvim" }
+
+  -- Syntax highlighting
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     -- To install: typescript javascript jsdoc lua luadoc haskell vue
-  }                                        -- Syntax highlighting
-  use { "p00f/nvim-ts-rainbow" }           -- TreeSitter brackets pair colorizer
-  use { "mattn/emmet-vim" }                -- Emmet
-  use { "nvim-lua/plenary.nvim" }          -- Required by Telescope
-  use { "nvim-telescope/telescope.nvim" }  -- Telescope (git, explorer)
-  use { "airblade/vim-gitgutter" }         -- Git changes file
+  }
+
+  -- Emmet
+  use { "mattn/emmet-vim" }
+
+  -- Required by Telescope
+  use { "nvim-lua/plenary.nvim" }
+
+  -- Telescope (git, explorer)
+  use { "nvim-telescope/telescope.nvim" }
+
+  -- Git changes file
+  use { "airblade/vim-gitgutter" }
+
+  -- Language server
   use {
     "neoclide/coc.nvim",
     branch = "release",
     run = ":CocInstall coc-highlight coc-prettier coc-eslint coc-sumneko-lua",
   }
-  -- use { "folke/neodev.nvim" }              -- Plugin development with full signature help, docs and completion for the nvim lua API
-  use { "folke/noice.nvim" }               -- Plugin which replace native Neovim command line
-  use { "MunifTanjim/nui.nvim" }           -- Required by Noice
-  use { "fladson/vim-kitty" }              -- Kitty configuration syntax highlighting
-end)
 
+  -- Plugin which replace native Neovim command line
+  use { "folke/noice.nvim" }
+
+  -- Required by Noice
+  use { "MunifTanjim/nui.nvim" }
+
+  -- Kitty configuration syntax highlighting
+  use { "fladson/vim-kitty" }
+
+  -- KMonad configuration syntax highlighting
+  use { 'kmonad/kmonad-vim' }
+end)
