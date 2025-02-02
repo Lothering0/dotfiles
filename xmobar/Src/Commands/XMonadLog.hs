7f6d22f6742ea9920d7cc2aa4af3845d9de4c496
Lothering0 <danil2005232@gmail.com>
2025-02-02 18:52:36 +0300

    Xmobar: small fix

--- xmobar/Src/Commands/XMonadLog.hs
+++ xmobar/Src/Commands/XMonadLog.hs
@@ -6,4 +6,3 @@ import           Xmobar
 -- | See https://xmonad.github.io/xmonad-docs/xmonad-contrib/XMonad-Hooks-StatusBar.html
 xmonadLogCommand :: Runnable
 xmonadLogCommand = Run XMonadLog
--- xmonadLogCommand = Run UnsafeXMonadLog
